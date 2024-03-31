d0b5212e5df9747ca7e54c1fc2dfd98730e7290c
Abhilash107 <abhilash00003108884@gmail.com>
2024-03-31 09:55:28 +0530

    31.03.24

---
+++ src/plusOne.java
@@ -0,0 +1,12 @@
+public class plusOne {
+    public static void main(String[] args) {
+        int[] a= {1,2,3};
+        int sum= 0;
+        for(int i=0;i<a.length;i++){
+            
+            
+        }
+
+    }
+
+}
