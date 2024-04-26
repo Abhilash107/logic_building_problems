9af4777291b52e135910af20714ea7c393a8c06c
Abhilash107 <abhilash00003108884@gmail.com>
2024-04-26 09:45:01 +0530

    2qns

--- src/findDiffrence.java
+++ src/findDiffrence.java
@@ -2,33 +2,48 @@ import java.util.Arrays;
 
 public class findDiffrence {
     public static void main(String[] args) {
+
+        System.out.println(findTheDifference("","w"));
         
     }
-    public char findTheDifference(String s, String t) {
+    public static char findTheDifference(String s, String t) {
 
-        if(s.length() < t.length()){
+        if(s.length() >= t.length()){
            System.exit(0);
-
         }
 
+        int j=0;
         char[] str1 = s.toCharArray();
         char[] str2 = t.toCharArray();
         Arrays.sort(str1);
         Arrays.sort(str1);
 
-        int i = str1.length-1;
-        int j = str1.length-1;
+        for (int i = 0; i < str2.length; i++) {
+            if(s.length()==0){
+                return (char)str2[0];
+            }
+            if(j==str1.length-1){
+                return (char)str2[str2.length-1];
+            }
 
-        while()
-
+            if(str2[i]!=str1[j]){
+                return (char)str2[i];
+            }else{
+                j++;
+            }
+  
+        }
         
-
-
-        
-
+        return (char)str2[str2.length-1];
 
 
 
+        //
+        // 
+        //
+
+        // int i = str1.length-1;
+        // int j = str1.length-1;
         
     }
 
---
+++ src/mergeStringAlternately.java
@@ -0,0 +1,23 @@
+public class mergeStringAlternately {
+
+    public static void main(String[] args) {
+        String s1 = "ab";
+        String s2 = "pqr";
+        System.out.println(mergeAlternately(s1, s2));
+
+       
+
+
+        
+        
+    }
+
+    public static String mergeAlternately(String word1, String word2) {
+        StringBuilder str = new StringBuilder("");
+        
+    
+        return str.toString();
+    
+    }
+
+}
