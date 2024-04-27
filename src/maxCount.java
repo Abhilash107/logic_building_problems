75b01c0cf4501be8e26a8018da8a43878961f17e
Abhilash107 <abhilash00003108884@gmail.com>
2024-04-27 10:48:37 +0530

    gg

---
+++ src/maxCount.java
@@ -0,0 +1,15 @@
+public class maxCount {
+    public static void main(String[] args) {
+        
+    }
+
+    public int maximumCount(int[] nums) {
+        int c=0;
+        int z =0;
+        for (int i = 0; i < nums.length; i++) {
+            if()
+            
+        }
+    }
+
+}
